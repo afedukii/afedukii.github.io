d0b181e81ce555c278ccdc4c7ea01112ab752f40
afedukii <101606500+afedukii@users.noreply.github.com>
2022-06-07 20:13:16 -0500

    Second commit

---
+++ js/description.js
@@ -0,0 +1,17 @@
+function global() {
+    setTimeout(function(){
+        document.getElementById("autoWrite1").style.visibility = "visible";
+    }, 1000);
+    setTimeout(function(){
+        document.getElementById("autoWrite2").style.visibility = "visible";
+    }, 2000);
+    setTimeout(function(){
+        document.getElementById("autoWrite3").style.visibility = "visible";
+    }, 3000);
+    setTimeout(function(){
+        document.getElementById("autoWrite4").style.visibility = "visible";
+    }, 4000);
+    setTimeout(function(){
+        document.getElementById("autoWrite5").style.visibility = "visible";
+    }, 5000);
+}
