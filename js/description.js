dc34d9f6752b2b5574a1c59e1380b3293b0b6535
afedukii <101606500+afedukii@users.noreply.github.com>
2022-06-09 12:35:05 -0500

    Add files via upload

--- js/description.js
+++ js/description.js
@@ -1,14 +1,15 @@
 function global() {
+    
     setTimeout(function(){
         document.getElementById("autoWrite1").style.visibility = "visible";
-    }, 1800);
+    }, 1100);
     setTimeout(function(){
         document.getElementById("autoWrite2").style.visibility = "visible";
-    }, 2750);
+    }, 2100);
     setTimeout(function(){
         document.getElementById("autoWrite3").style.visibility = "visible";
-    }, 3750);
+    }, 3100);
     setTimeout(function(){
         document.getElementById("autoWrite4").style.visibility = "visible";
-    }, 4750);
+    }, 4100);
 }
