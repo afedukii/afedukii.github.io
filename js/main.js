65e90516e847c708ee33abac459f7fa92d757e88
afedukii <101606500+afedukii@users.noreply.github.com>
2022-06-10 17:36:53 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+resizeWindow();
+window.addEventListener('resize', resizeWindow);
+function resizeWindow(){
+    if(window.innerWidth < 770){
+        document.getElementById("header_cont_namepage").classList.remove("col-sm-6");
+        document.getElementById("header_cont_namepage").classList.add("col-sm-12");
+    }else{
+        document.getElementById("header_cont_namepage").classList.remove("col-sm-12");
+        document.getElementById("header_cont_namepage").classList.add("col-sm-6");
+    }
+}
+
+
 const typed = new Typed('.typed', {
 	stringsElement: '#knowledge_pres', // ID del elemento que contiene cadenas de texto a mostrar.
 	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
@@ -5,7 +18,7 @@ const typed = new Typed('.typed', {
 	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
-	backDelay: 2000, // Tiempo de espera despues de que termina de escribir una palabra.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
 	loop: true, // Repetir el array de strings
 	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
 	showCursor: true, // Mostrar cursor palpitanto
