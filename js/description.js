74315d87b93304ef3f3854e6cb9c98798c67a23e
afedukii <101606500+afedukii@users.noreply.github.com>
2022-06-09 19:58:18 -0500

    Add files via upload

--- js/description.js
+++ js/description.js
@@ -13,3 +13,19 @@ function global() {
         document.getElementById("autoWrite4").style.visibility = "visible";
     }, 4100);
 }
+resizeWindow();
+window.addEventListener('resize', resizeWindow);
+
+function resizeWindow(){
+    if(window.innerWidth < 710){
+        document.getElementById("deskMenuLogo").classList.remove("col-sm-6");
+        document.getElementById("deskMenuLogo").classList.add("col-sm-12");
+        document.getElementById("textPres").classList.remove("col-sm-6");
+        document.getElementById("textPres").classList.add("col-sm-12");
+    }else{
+        document.getElementById("deskMenuLogo").classList.remove("col-sm-12");
+        document.getElementById("deskMenuLogo").classList.add("col-sm-6");
+        document.getElementById("textPres").classList.remove("col-sm-12");
+        document.getElementById("textPres").classList.add("col-sm-6");
+    }
+}
