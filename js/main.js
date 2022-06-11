0dc07a722f80d75b88ac430b0a4a18dd23aa137d
afedukii <101606500+afedukii@users.noreply.github.com>
2022-06-11 13:01:35 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,11 +12,29 @@ const typed = new Typed('.typed', {
 	cursorChar: '_', // Caracter para el cursor
 	contentType: 'html', // 'html' o 'null' para texto sin formato
 });
-
+const typed2 = new Typed('.typed2', {
+	stringsElement: '#knowledge_pres_es', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 50, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 1000, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 50, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '_', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+const textsToChange = document.querySelectorAll("[data-section]");
 const changeLanguage = async (language) => {
 	const requesJson = await fetch('https://afedukii.github.io/languages/'+language+'.json');
 	const texts = await requesJson.json();
-	console.log(texts);
+	for(const textToChange of textsToChange){
+		const section = textToChange.dataset.section;
+		const value = textToChange.dataset.value;
+		textToChange.innerHTML = texts[section][value];
+	}
 };
 
 resizeWindow();
@@ -39,6 +57,13 @@ window.onload = function () {
     for (i = 0; i < flagsElements.length; i++) {
     	flagsElements[i].addEventListener("click",(e)=>{
 			changeLanguage(e.target.parentElement.dataset.language);
+			if(e.target.parentElement.dataset.language == "en"){
+				document.getElementById('es_presentation').style.visibility = 'hidden';
+				document.getElementById('en_presentation').style.visibility = 'visible';
+			}else{
+				document.getElementById('en_presentation').style.visibility = 'hidden';
+				document.getElementById('es_presentation').style.visibility = 'visible';
+			}
     	});
     }
 }
