438ad15e5fb419f8629be1b50456aae5878c339a
Alfredo Moreno <101606500+afedukii@users.noreply.github.com>
2022-06-17 16:48:55 -0500

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,31 +1,4 @@
-const typed = new Typed('.typed', {
-	stringsElement: '#knowledge_pres', // ID del elemento que contiene cadenas de texto a mostrar.
-	typeSpeed: 50, // Velocidad en mlisegundos para poner una letra,
-	startDelay: 1000, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
-	backSpeed: 50, // Velocidad en milisegundos para borrrar una letra,
-	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
-	shuffle: false, // Alterar el orden en el que escribe las palabras.
-	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
-	loop: true, // Repetir el array de strings
-	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
-	showCursor: true, // Mostrar cursor palpitanto
-	cursorChar: '_', // Caracter para el cursor
-	contentType: 'html', // 'html' o 'null' para texto sin formato
-});
-const typed2 = new Typed('.typed2', {
-	stringsElement: '#knowledge_pres_es', // ID del elemento que contiene cadenas de texto a mostrar.
-	typeSpeed: 50, // Velocidad en mlisegundos para poner una letra,
-	startDelay: 1000, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
-	backSpeed: 50, // Velocidad en milisegundos para borrrar una letra,
-	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
-	shuffle: false, // Alterar el orden en el que escribe las palabras.
-	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
-	loop: true, // Repetir el array de strings
-	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
-	showCursor: true, // Mostrar cursor palpitanto
-	cursorChar: '_', // Caracter para el cursor
-	contentType: 'html', // 'html' o 'null' para texto sin formato
-});
+
 const textsToChange = document.querySelectorAll("[data-section]");
 const changeLanguage = async (language) => {
 	const requesJson = await fetch('https://afedukii.github.io/languages/'+language+'.json');
@@ -36,6 +9,24 @@ const changeLanguage = async (language) => {
 		textToChange.innerHTML = texts[section][value];
 	}
 };
+
+
+window.onload = function () {
+	$('#loader').fadeOut();
+	$('body').removeClass('hidden');
+    var flagsElements = document.querySelectorAll('.flags')
+    for (i = 0; i < flagsElements.length; i++) {
+    	flagsElements[i].addEventListener("click",(e)=>{
+			changeLanguage(e.target.parentElement.dataset.language);
+    	});
+    }
+	if(window.innerWidth < 770){
+		document.getElementById('navBarHead').style.zIndex = 5;
+		document.getElementById('navBarBottom').style.zIndex = 5;
+    }else{		
+		document.getElementById('navBarHead').style.zIndex = 5;
+    }
+}
 
 resizeWindow();
 window.addEventListener('resize', resizeWindow);
@@ -66,26 +57,14 @@ function resizeWindow(){
 		document.getElementById('navBarHead').style.zIndex = 5;
     }
 }
-window.onload = function () {
-	$('#loader').fadeOut();
-	$('body').removeClass('hidden');
-    var flagsElements = document.querySelectorAll('.flags')
-    for (i = 0; i < flagsElements.length; i++) {
-    	flagsElements[i].addEventListener("click",(e)=>{
-			changeLanguage(e.target.parentElement.dataset.language);
-			if(e.target.parentElement.dataset.language == "gb"){
-				document.getElementById('i-es').style.display = 'none';
-				document.getElementById('i-en').style.display = 'block';
-			}else{
-				document.getElementById('i-en').style.display = 'none';
-				document.getElementById('i-es').style.display = 'block';
-			}
-    	});
-    }
-	if(window.innerWidth < 770){
-		document.getElementById('navBarHead').style.zIndex = 5;
-		document.getElementById('navBarBottom').style.zIndex = 5;
-    }else{		
-		document.getElementById('navBarHead').style.zIndex = 5;
-    }
+/* icon bar rotation */
+function rotateIcon(x) {
+    x.classList.toggle("change");
+	var mobileMenu = document.getElementById("mobileMenu");
+	if(mobileMenu.style.display == "block"){
+		mobileMenu.style.display = "none";
+	}else{
+		mobileMenu.style.display = "block";
+	}
 }
+
